01bd8c7eb910ff695c2d1a29e76af2acfaecef71
Kristján Pétursson <kristjan@gmail.com>
2012-05-02 16:17:26 -0700

    Wire up Twitter to check for links
    Since there's no aggregate `/types/links` endpoint yet (see Singly/hallway#162),
    let's start with just incoming tweets. Future work:
    
    * Checking your own tweets - I hope you remember posting things
    * Checking Facebook
    
    The extension needed its Content Security Policy tweaked to allow JSON calls to
    the Singly API.

---
+++ js/background.js
@@ -0,0 +1,6 @@
+function onRequest(request, sender, callback) {
+  if (request.action == 'getJSON') {
+    $.getJSON(request.url, callback);
+  }
+}
+chrome.extension.onRequest.addListener(onRequest);
--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,63 @@
+PostFirst = (function() {
+  var API_HOST = 'https://carealot.singly.com';
+
+  function check(tab) {
+    $('#checking .url').text(tab.title);
+    fetchLinks(function(links) {
+      var link = links[tab.url];
+      if (link) {
+        fillPosters(link);
+        $('#checking, #already_posted').toggle();
+      } else {
+        $('#checking, #post').toggle();
+      }
+    });
+  }
+
+  function fetchLinks(done) {
+    $.getJSON(api('/services/twitter/timeline', {
+      limit: 300
+    }), function(tweets) {
+      var links = {};
+      _.each(tweets, function(tweet) {
+        var urls = tweet.data.entities.urls;
+        _.each(urls, function(url) {
+          url = url.expanded_url;
+          console.log(tweet, url);
+          if (url) {
+            if (!links[url]) links[url] = [];
+            links[url].push({
+              name: tweet.data.user.name
+            });
+          }
+        });
+      });
+      done(links);
+    }).error(error);
+  }
+
+  function fillPosters(posters) {
+    var list = $('#already_posted .posters');
+    _.each(posters, function(poster) {
+      list.append($('<li>').text(poster.name));
+    });
+  }
+
+  function api(path, options) {
+    if (!options) options = {};
+    options.access_token = auth.getAccessToken();
+    return API_HOST + path + '?' + $.param(options);
+  }
+
+  function error() {
+    $('#checking, #error').toggle();
+  }
+
+  return {
+    check : check
+  };
+})();
+
 var auth = new OAuth2('singly', {
   client_id: 'postfirst',
   client_secret: '46486d124edb2bafa8f7f429a91cd',
@@ -5,7 +65,7 @@ var auth = new OAuth2('singly', {
 });
 
 auth.authorize(function() {
-  $('#content').append(
-    $('<p>').text(auth.getAccessToken())
-  );
+  chrome.tabs.getSelected(null, function(tab) {
+    PostFirst.check(tab);
+  });
 });
