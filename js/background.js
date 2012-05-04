e67183fa36dc93bf670b2cfff05a863174862a86
Kristján Pétursson <kristjan@gmail.com>
2012-05-04 10:51:09 -0700

    Remove unused js/background.js
    This was for an RPC tunnel to avoid CSP issues, but the CSP problems got worked
    out.

--- js/background.js
+++
@@ -1,6 +0,0 @@
-function onRequest(request, sender, callback) {
-  if (request.action == 'getJSON') {
-    $.getJSON(request.url, callback);
-  }
-}
-chrome.extension.onRequest.addListener(onRequest);
